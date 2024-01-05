fe539d1e655de1b08796e732bcd691ebdf6eaaa5
amoskarugo <karugoamos@gmail.com>
2024-01-06 01:41:05 +0300

    #

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -76,3 +76,9 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """
+        calls the reload function and closes the database
+        """
+        self.reload()
